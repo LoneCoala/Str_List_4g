c23f65d959f56aa3b18d18c98094f17138a69cfa
Даниил Тюрин <tjurin.daniil@rambler.ru>
2022-02-23 08:06:10 +0700

    Добавление элементов в список пользователем

--- Str_List_4e/Program.cs
+++ Str_List_4e/Program.cs
@@ -7,19 +7,52 @@ namespace Str_List_4g
     {
         static void Main(string[] args)
         {
-            List<string> people = new List<string>() { "Tom", "Bob", "Sam" , "Aaa", "12321","Але","ксандр"};
+            List<string> people = new List<string>() { "Tom", "Bob", "Sam"};
+            Console.WriteLine("Начальный список:");
             for (int i = 0; i < people.Count; i++)
             {
                 Console.Write(people[i] + " ");
             }
+            Console.WriteLine("Сколько элементов добавить в список?");
+            int n = 0;
 
-            RevList(people);
+            while(!int.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("Введена неверная форма числа! Попробуйте снова.");
+            }
 
-            Console.WriteLine("\nНовый список:");
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write($"Введите новый элемент списка №{i+1}:");
+                people.Add(Console.ReadLine());
+                Console.WriteLine("");
+            }
+
+            Console.WriteLine("Новый список до изменений:");
             for (int i = 0; i < people.Count; i++)
             {
                 Console.Write(people[i] + " ");
             }
+            Console.WriteLine("\nУдалить последний элемент или закончить выполнение программы?\n1.Удалить\n0.Закончить");
+            while (Console.ReadLine() == "1")
+            {
+                if(people.Count == 0)
+                {
+                    Console.WriteLine("В списке закончились элементы.");
+                    break;
+                }
+                
+                RevList(people);
+
+                Console.WriteLine("\nНовый список:");
+                for (int i = 0; i < people.Count; i++)
+                {
+                    Console.Write(people[i] + " ");
+                }
+                Console.WriteLine("\nУдалить последний элемент или закончить выполнение программы?\n1.Удалить\n0.Закончить");
+            }
+
+            Console.WriteLine("Конец работы программы");
 
             List<string> RevList(List<string> list)
             {
